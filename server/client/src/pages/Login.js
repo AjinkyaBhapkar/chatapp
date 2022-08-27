34eb130472d22e70e306094a85694f61877aca1e
Ajinkya Bhapkar <101281801+AjinkyaBhapkar@users.noreply.github.com>
2022-08-27 21:26:05 +0530

    Update Login.js

--- server/client/src/pages/Login.js
+++ server/client/src/pages/Login.js
@@ -4,24 +4,39 @@ import hero from '../images/4112338.jpg'
 
 const Login = () => {
     const [toggle,setToggle]=useState('login')
-  return (
+
+    
+
+  return (<><h2 className='text-center font-mono text-5xl text-green-700 font-extrabold italic  pt-5 -mb-5'>Tingg!!</h2>
     <div className={`h-screen w-screen py-2 flex lg:flex-row  lg:justify-between justify-evenly flex-col items-center `}>
+        
         <div className=' lg:w-1/2 lg:h-auto  lg:m-10 w-full my-5 '>
             <img  src={hero} alt="" />
         </div>
         <div className='lg:w-1/3  w-11/12 my-10 lg:m-10 mx-auto  border-2 border-gray-600 p-2 rounded-lg bg-gray-400' >
-            <h3 className='text-center text-4xl text-white py-5 font-bold'>Join Chat</h3>
+            
+                
+            <div className='flex'>
+                <button 
+                onClick={()=>{setToggle('login')}}
+                className={`bg-transparent text-lg ${(toggle==='login')? 'font-light':'font-semibold'} w-1/2`}>Login</button>
+                <button 
+                onClick={()=>{setToggle('signup')}}
+                className={`bg-transparent text-lg ${(toggle==='login')? 'font-semibold':'font-light'} w-1/2`}>Sign-up</button>
+            </div>
             
             <form className='flex flex-col py-4 text-white'>
                 <input className='focus:outline-0 m-1 p-1 bg-gray-600 ' type="text" name="" id="" placeholder='Username' />
-                <input className='focus:outline-0 m-1 p-1 bg-gray-600 ' type="text" name="" id="" 
-                placeholder='Invite ID' />
+                <input className='focus:outline-0 m-1 p-1 bg-gray-600 ' type="Password" name="" id="" 
+                placeholder='Password' />
+                <input className={`focus:outline-0 m-1 p-1 bg-gray-600 ${(toggle==='login')?'hidden':''} `} type="text" name="" id="" 
+                placeholder='Confirm Password' />
                 <input type="submit" value="Join" className='bg-gray-600 mx-1 my-3 p-1 border-2 border-gray-900 text-xl'/>
             </form>
         </div>
 
       
-    </div>
+    </div></>
   )
 }
 
