96ca515f6fd6b77902842c50376cd608d693857c
Ajinkya Bhapkar <101281801+AjinkyaBhapkar@users.noreply.github.com>
2022-09-08 21:26:17 +0530

    redux-persist added

--- server/client/src/app/store.js
+++ server/client/src/app/store.js
@@ -1,9 +1,23 @@
 import { configureStore, current } from '@reduxjs/toolkit'
 import userReducer from '../features/user/userSlice'
+import {persistReducer ,persistStore} from "redux-persist"
+import storage from 'redux-persist/lib/storage'
+import thunk from 'redux-thunk'
+
+const persistConfig ={
+  key:'chatApp',
+  storage
+}
+
+const persistedReducer=persistReducer(persistConfig,userReducer)
 
 
 export const store = configureStore({
-  reducer: {
-    user: userReducer
-  },
+  reducer:{
+    user:persistedReducer,
+  } ,
+    
+  middleware:[thunk]
 })
+
+export const persistor =persistStore(store)
